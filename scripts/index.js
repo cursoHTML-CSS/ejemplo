0af1f20a3f4169cca7beb5716bdfe6043782c794
nicki182 <nicky.stru@gmail.com>
2021-04-03 14:52:54 -0300

    simple validator
    only checks if you completed form if not button submit is disabled

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,40 @@
-function validateInput(){
-    console.log(document.getElementById('nameExample').focus);
+function validateInput(){  
+    if(document.getElementById('nameExample').value===''){
+            showRequired('errorName','nameExample');
+    }else{
+        validate();
+        hideRequired('errorName','nameExample');
+    }
 }
-function validateEmail(e){
-
+function validateTextArea(){
+    if(document.getElementById('escribirExample').value===''){
+        showRequired('errorEscribir','escribirExample');
+}else{
+    validate();
+    hideRequired('errorEscribir','escribirExample');
 }
-function validate(e){
-
+}
+function showRequired(elementIdError,elementId){
+    document.getElementById(elementIdError).innerText='Required';
+    document.getElementById(elementId).style.border='#9A0033 solid 2px';
+}
+function hideRequired(elementIdError,elementId){
+    document.getElementById(elementIdError).innerText='';
+    document.getElementById(elementId).style.border='none';
+}
+function validateCheckBoxRadio(){
+    validate();
+}
+function validate(){
+//Check if within the form there is an element that is not completed
+    const radioButtons =[...document.getElementsByName('radioExample')];
+      const checkBoxes=[...document.getElementsByName("checkboxExample")];
+      const inputs=[...document.getElementsByName("exampleInput")];
+      const textAreas=[...document.getElementsByName("exampleTextArea")];
+      if (radioButtons.some(radioButton=>radioButton.checked===true) && inputs.some(input=>input.value!=="") && textAreas.some(textArea=>textArea.value!=="") && checkBoxes.some(checkBox=>checkBox.checked===true) ) {  
+          document.getElementById("submitButton").disabled=false;
+      }
+}
+function onSubmit(){
+    console.log('success');
 }
